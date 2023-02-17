253b3348f0e1d1e9064e8cbc069d5b3315159a75
jonnyk17 <jonnyk.pda7@gmail.com>
2023-02-17 17:26:31 -0500

    Fibonacci and binary search
    Fibonacci and binary search algorithms in kotlin

--- main.kt
+++ main.kt
@@ -1,3 +1,33 @@
-fun main(){
+import java.util.*
+
+fun fib(n: Int): Int{
     
+    if(n<=1){
+        return n
+    }
+    return fib(n-1)+fib(n-2)
+}
+fun binary_search(a: List<Int>, low:Int, high:Int, n:Int): Int{
+    if(high>=low){
+        var mid:Int= low+(high-low)/2
+        if(a[mid]==n)
+            return mid
+        if(a[mid]>n)
+            return binary_search(a, low, mid-1, n)
+        
+        return binary_search(a, mid+1, low, n)
+    }
+    return -1
+}
+
+fun main(){
+    val reader = Scanner(System.`in`)
+    println("Enter a number:")
+    var n:Int = reader.nextInt()
+    println(fib(n))
+    val a = List(10) { Random().nextInt(20) }
+    val num = Random().nextInt(20)
+    println(a)
+    println(num)
+    println(binary_search(a, 0, 9, num))
 }
